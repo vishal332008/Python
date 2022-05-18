c51010bb68a17512c663dc042186b4c0b630143e
Vishal <76160371+vishal332008@users.noreply.github.com>
2022-05-18 10:43:09 +0530

    Create main.py

---
+++ WEB SCRAPING 2/main.py
@@ -0,0 +1,28 @@
+from bs4 import BeautifulSoup as bs
+import requests
+import pandas as pd
+
+url = 'https://en.wikipedia.org/wiki/List_of_brown_dwarfs'
+
+page = requests.get(url)
+soup = bs(page.text,'html.parser')
+star_table = soup.find_all('table')
+temp_list= []
+for tr in star_table[5].find_all('tr'):
+    td = tr.find_all('td')
+    row = [i.text.rstrip() for i in td]
+    temp_list.append(row)
+    
+names = []
+Distance =[]
+Mass = []
+Radius =[]
+for i in range(1,len(temp_list)):
+    names.append(temp_list[i][0])
+    Distance.append(temp_list[i][5])
+    Mass.append(temp_list[i][7])
+    Radius.append(temp_list[i][8])
+
+df = pd.DataFrame(list(zip(names,Distance,Mass,Radius)),columns=['Star_name','Distance','Mass','Radius'])
+
+df.to_csv('dwarfs.csv')
